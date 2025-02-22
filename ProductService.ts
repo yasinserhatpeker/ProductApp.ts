1cb71c819b0c42f4854abdc9eb91eca26df667d2
Yasin <yasinpeker43@gmail.com>
2025-02-22 15:05:06 +0300

    ProductService's method are rewritten.

--- ProductService.ts
+++ ProductService.ts
@@ -1,18 +1,49 @@
 import { IProductService } from "./IProductService";
 import { Product } from "./Product";
+import { SimpleProductData } from "./SimpleProductData";
 
 class ProductService implements IProductService {
+
+    private dataSource:SimpleProductData
+    private products:Array<Product>
+
+    constructor() {
+        this.dataSource=new SimpleProductData();
+        this.products=new Array <Product>();
+        this.dataSource.getProducts().forEach(p=> this.products.push(p));
+    }
     getById(id: number): Product {
-        throw new Error("Method not implemented.");
+        return this.products.filter(p=>p.id ===id)[0];
     }
     getProducts(): Array<Product> {
-        throw new Error("Method not implemented.");
+        return this.products;
     }
     saveProduct(product: Product): void {
-        throw new Error("Method not implemented.");
+       if(product.id==0 || product.id==null) {
+        product.id=this.generateId();
+        this.products.push(product);
+       }
+       else {
+        let index=this.products.indexOf(product);
+        this.products.splice(index,1,product);
+       }
     }
     deleteProduct(product: Product): void {
-        throw new Error("Method not implemented.");
+       let index=this.products.indexOf(product);
+       if(index>0) {
+        this.products.splice(index,1);
+        
+       }
+    }
+
+    private generateId():number {
+        let key=1;
+        while(this.getById(key)!=null) {
+              key++;
+        }
+        return key;
+
+
     }
  
 }
